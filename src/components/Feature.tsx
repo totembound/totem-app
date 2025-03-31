1c3b4376a0ca44eede83adb08a074821864ebb16
starman69 <dave@pattens.org>
2025-03-30 20:57:45 -0700

    update alignment

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -6,7 +6,7 @@ export const Feature: React.FC<{
     large?: boolean;
 }> = ({ icon, text, large }) => (
     <div className="flex items-start gap-3">
-        <span className={`${large ? 'text-xl' : 'text-lg'} mt-1`}>{icon}</span>
+        <span className={`${large ? 'text-xl' : 'text-lg'} mb-1`}>{icon}</span>
         <p className={`${large ? 'text-xl' : 'text-base md:text-lg'} text-gray-700 dark:text-gray-300`}>
             {text}
         </p>

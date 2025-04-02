a9e56c3e74ac2c36170b15eb1045d2996e4bf31d
Brandon <brandon@pattens.org>
2025-04-02 12:59:31 -0700

    feat/ringdive (#25)
    * added images used in first agility challenge
    
    * added first agility challenge
    
    * changed file to use SprirtPathChallenge
    
    * cleaned up obsolete code and fixed mobile view
    
    * fixed container width issue
    
    * changed panel image and description for first agility challenge
    
    * changed background image name to be more semantic
    
    * added images used in first wisdom challenge
    
    * created first wisdom challenge
    
    * added first wisdom challenge to be played
    
    * fixed timer not resuming bug
    
    * changes made to gameplay and bug fixes
    
    * tweaked difficulty
    
    * added images for second agility challenge
    
    * created second agility challenge
    
    * added second agility test to be used
    
    * created component and hook as well as a test to create floating messages
    
    * removed debug mode and fixed character position issue
    
    * renamed ScoreMessageEffect
    
    * changed name to be more semantic
    
    * changed name used to match file name

---
+++ src/hooks/useScoreMessagesEffect.ts
@@ -0,0 +1,47 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+import { ScoreMessage } from '../components/effects/ScoreMessageEffect';
+
+export const useScoreMessages = (duration = 1000) => {
+  const [scoreMessages, setScoreMessages] = useState<ScoreMessage[]>([]);
+  const scoreMessagesRef = useRef<ScoreMessage[]>([]);
+  
+  // Add a new score message
+  const addScoreMessage = useCallback((x: number, y: number, value: number) => {
+    const newMessage: ScoreMessage = {
+      id: Date.now(),
+      value,
+      x,
+      y,
+      createdAt: Date.now()
+    };
+    
+    scoreMessagesRef.current = [...scoreMessagesRef.current, newMessage];
+    setScoreMessages(scoreMessagesRef.current);
+  }, []);
+
+  // Update and clean up score messages
+  useEffect(() => {
+    const messageInterval = setInterval(() => {
+      const now = Date.now();
+      
+      // Filter out messages that have expired
+      const updatedMessages = scoreMessagesRef.current.filter(
+        message => now - message.createdAt < duration
+      );
+      
+      if (updatedMessages.length !== scoreMessagesRef.current.length) {
+        scoreMessagesRef.current = updatedMessages;
+        setScoreMessages(updatedMessages);
+      }
+    }, 100);
+    
+    return () => {
+      clearInterval(messageInterval);
+    };
+  }, [duration]);
+
+  return {
+    scoreMessages,
+    addScoreMessage
+  };
+};

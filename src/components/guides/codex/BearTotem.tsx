20055509213ab35f927309bb15c665b9550b7617
Dave Patten <dave@pattens.org>
2026-03-29 17:36:50 -0700

    feat/bear (#119)
    * add bear totem
    
    * update shop and roadmap for bear
    
    * remove unused  species data

--- src/components/guides/codex/BearTotem.tsx
+++ src/components/guides/codex/BearTotem.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { TotemView } from "./TotemView";
 import { Species } from "../../../types/types";
+import { useCodexVariants } from "../../../utils/species";
 
 const BearTotem: React.FC = () => {
+    const variants = useCodexVariants(Species.Bear);
     return <TotemView config={{
       species: Species.Bear,
-      variants: []
+      variants
     }}/>
 };
 

7321e188584bb8de8725a352ed995f12890b0ca3
starman69 <dave@pattens.org>
2026-05-25 21:24:32 -0700

    fix raven codex variants

--- src/components/guides/codex/RavenTotem.tsx
+++ src/components/guides/codex/RavenTotem.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { TotemView } from "./TotemView";
 import { Species } from "../../../types/types";
-
+import { useCodexVariants } from "../../../utils/species";
 
 const RavenTotem: React.FC = () => {
+    const variants = useCodexVariants(Species.Raven);
     return <TotemView config={{
       species: Species.Raven,
-      variants: []
+      variants
     }}/>
 };
 
